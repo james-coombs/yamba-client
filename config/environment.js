9f3c97ab4bf2d276c5be855521d40f2cff5c9ab9
james-coombs <coombsj4@yahoo.com>
2016-11-08 16:46:27 -0500

    Set enviorment production vars
    Set enviorment production vars to the same as gh-pages branch.
    Deploys are hard.

--- config/environment.js
+++ config/environment.js
@@ -49,7 +49,7 @@ module.exports = function (environment) {
   }
 
   if (environment === 'production') {
-    ENV.rootURL = '/';
+    ENV.rootURL = '/wember_client';
     ENV.locationType = 'hash';
     ENV.apiHost = 'https://wember.herokuapp.com';
   }
