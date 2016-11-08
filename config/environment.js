5c272567ec3d0d14d73648df15fdea6e2b52288a
james-coombs <coombsj4@yahoo.com>
2016-11-08 14:02:58 -0500

    Change 'baseURL' to 'rootURL'

--- config/environment.js
+++ config/environment.js
@@ -49,7 +49,7 @@ module.exports = function (environment) {
   }
 
   if (environment === 'production') {
-    ENV.baseURL = '/';
+    ENV.rootURL = '/';
     ENV.locationType = 'hash';
     ENV.apiHost = 'https://wember.herokuapp.com';
   }
