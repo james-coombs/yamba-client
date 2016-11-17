b80a5f92a86fb11d6bbee0315bedc9c82b197dc4
james-coombs <coombsj4@yahoo.com>
2016-11-17 09:38:05 -0500

    Add in image upload feature
    Add service for uploads.
    Un comment out code allowing uploads.

--- app/server/route.js
+++ app/server/route.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-//  uploads: Ember.inject.service(),
+uploads: Ember.inject.service(),
   model(params) {
     return this.get('store').findRecord('server', params.server_id);
   },
@@ -12,12 +12,12 @@ export default Ember.Route.extend({
     },
 
     createPage (newPage) {
-      let createdPage = this.get('store').createRecord('page', newPage);
-      createdPage.save();
-      // We can console log the newMovie object to ensure it is being passed through
-      //  return this.get('uploads').newPageUpload(newPage) // This will use the uploads service, which gives us access to the `newMovieUpload` function.
-      // .then(() => this.refresh()) // Once the upload is successful, we will transition to our list of movies and if all worked, we should see the newly created movie
-      // .catch((error) => console.error(error));
+      // let createdPage = this.get('store').createRecord('page', newPage);
+      // createdPage.save();
+
+       return this.get('uploads').newPageUpload(newPage) // This will use the uploads service, which gives us access to the `newPageUpload` function.
+      .then(() => this.refresh()) // Once the upload is successful, we will transition to our list of pages and if all worked, we should see the newly created movie
+      .catch((error) => console.error(error));
     },
   }
 });
